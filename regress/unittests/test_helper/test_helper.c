3db1220ee9a72d196bd6d425eaa29f7935c167e8
Juliusz Sosinowicz <juliusz@wolfssl.com>
2020-02-26 22:55:05 +0100

    Import ssl.h

--- regress/unittests/test_helper/test_helper.c
+++ regress/unittests/test_helper/test_helper.c
@@ -38,6 +38,7 @@
 #ifdef WITH_OPENSSL
 #include <openssl/bn.h>
 #include <openssl/err.h>
+#include <openssl/ssl.h>
 #endif
 
 #if defined(HAVE_STRNVIS) && defined(HAVE_VIS_H) && !defined(BROKEN_STRNVIS)
